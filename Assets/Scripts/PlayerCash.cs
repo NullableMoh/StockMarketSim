2b55ae24cbedd35f1e6ee988f38942314e2eab72
NullableMoh <51676735+NullableMoh@users.noreply.github.com>
2022-12-29 22:52:17 -0500

    Initial Commit
    Initial Commit

---
+++ Assets/Scripts/PlayerCash.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCash : MonoBehaviour
+{
+	[SerializeField] float startingCash;
+	
+	BuyButton[] buyButtons;
+	float currentCash;
+	
+	void Awake()
+	{
+		currentCash = startingCash;
+	}
+	
+	void OnEnable()
+	{
+		buyButtons = FindObjectsOfType<BuyButton>();
+		foreach(var buyButton in buyButtons)
+			buyButton.OnBuyButtonClicked += CheckIfEnoughCashToBuyStock;
+	}
+	
+	void OnDisable()
+	{
+		foreach(var buyButton in buyButtons)
+			buyButton.OnBuyButtonClicked -= CheckIfEnoughCashToBuyStock;		
+	}
+	
+	void CheckIfEnoughCashToBuyStock(float stockPrice)
+	{
+		Debug.Log("Recieved Stock Price: " + stockPrice);
+	}
+}
---
+++ Assets/Scripts/StockPresenter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StockPresenter : MonoBehaviour
+{
+	TextMeshProUGUI text;
+	Stock stock;
+	
+	void OnEnable()
+	{
+		stock = GetComponent<Stock>();
+		stock.OnStockPriceUpdated += UpdateStockUI;		
+		
+		text = GetComponent<TextMeshProUGUI>();
+	}
+	
+	void OnDisable()
+	{
+		stock.OnStockPriceUpdated -= UpdateStockUI;		
+	}
+	
+	void UpdateStockUI(string stockName, float currentStockPrice, float stockHoldings)
+	{
+		text.text = $"{stockName}: ${currentStockPrice:0.00}\n" +
+					$"HOLDINGS: ${stockHoldings:0.00}";
+	}
+
+	
+}
